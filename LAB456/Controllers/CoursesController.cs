8e63639bf529d4a24f080040cb089c66ed37c358
hoanganh5 <hoanganh51199@gmail.com>
2020-06-08 10:13:41 +0700

    b

--- LAB456/Controllers/CoursesController.cs
+++ LAB456/Controllers/CoursesController.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿using LAB456.Models;
+using LAB456.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 namespace LAB456.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
         // GET: Courses
-        public ActionResult Index()
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+       
+        public ActionResult Create()
         {
             return View();
         }
