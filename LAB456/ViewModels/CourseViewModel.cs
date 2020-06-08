e9a4a4ee4314513958046e9585d8ad9584303276
hoanganh5 <hoanganh51199@gmail.com>
2020-06-08 10:04:41 +0700

    h

---
+++ LAB456/ViewModels/CourseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using LAB456.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LAB456.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+        [Required]
+        public string Date { get; set; }
+        [Required]
+        public string Time { get; set; }
+        [Required]
+
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+} 
